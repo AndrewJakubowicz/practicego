a336174fb1550c3a2ed325f1be3eac28539da92b
SpyR1014 <spyr1014@gmail.com>
2016-01-23 21:45:55 +1100

    Added a struct example

--- 08_pointers/main.go
+++ 08_pointers/main.go
@@ -6,6 +6,11 @@ func zero(z *int) {
 	*z = 0
 }
 
+type Vertex struct {
+	X int
+	Y int
+}
+
 func main() {
 
 	a := 43
@@ -21,11 +26,17 @@ func main() {
 	*b = 42 // reassign value of b to 42 which changes a.
 	fmt.Println(a)
 
-
 	// Check out this amazing function.
 	x := 11
 	fmt.Println(x)
 	zero(&x)
 	fmt.Println(x)
+
+	// Struct example
+	// reference struct through a pointer
+	v := Vertex{1, 2}
+	p := &v
+	p.X = 1e9
+	fmt.Println(v)
 
 }
