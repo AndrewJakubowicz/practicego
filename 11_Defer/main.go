0a4c5580f4e0cbfcc13ea0f604c97038aef8689d
SpyR1014 <spyr1014@gmail.com>
2016-01-23 21:59:39 +1100

    Adding slice example, with exercise and a defer example.

---
+++ 11_Defer/main.go
@@ -0,0 +1,15 @@
+// Example of defer.
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("counting")
+
+	for i := 0; i < 10; i++ {
+		defer fmt.Println(i)
+	}
+	fmt.Println("done")
+}
+
+// Defer executes as a last in, first out style.
