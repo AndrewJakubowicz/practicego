70e410108944f97e431c5e8bb59b0adb02d4a2e1
SpyR1014 <spyr1014@gmail.com>
2016-01-23 17:35:54 +1100

    Pointer example added.

---
+++ 08_pointers/main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func zero(z *int) {
+	*z = 0
+}
+
+func main() {
+
+	a := 43
+
+	fmt.Println(a)
+	fmt.Println(&a)
+
+	var b *int = &a // the type is a pointer to an int. Points to a memory address where an int is stored.
+
+	fmt.Println(b)
+	fmt.Println(*b) // de-referenced so returns 43
+
+	*b = 42 // reassign value of b to 42 which changes a.
+	fmt.Println(a)
+
+
+	// Check out this amazing function.
+	x := 11
+	fmt.Println(x)
+	zero(&x)
+	fmt.Println(x)
+
+}
