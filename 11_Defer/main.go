0fb3afa729230470974521aa63ae56a1fd0bff23
SpyR1014 <spyr1014@gmail.com>
2016-01-27 19:49:08 +1100

    Added a bunch more useful examples. Also ammended Defer slightly.

--- 11_Defer/main.go
+++ 11_Defer/main.go
@@ -13,3 +13,10 @@ func main() {
 }
 
 // Defer executes as a last in, first out style.
+// Anything with defer will run when the function it is within (main()) in this case,
+// is about to exit.
+
+// First counting is printed.
+// "done" is printed
+// Now main() is about to exit.
+// Defer functions run. Last defer call runs first.
