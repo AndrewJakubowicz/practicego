424e80631c45b48e7880b0f1634bf18c1328a8d0
SpyR1014 <spyr1014@gmail.com>
2016-01-30 14:23:41 +1100

    Array example

---
+++ 21_array/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var x [58]string // if this was int it would put a 0 in every position.
+	fmt.Println(x)
+	fmt.Println(len(x))
+	fmt.Println(x[42])
+	for i := 65; i <= 122; i++ {
+		x[i-65] = string(i)
+	}
+	fmt.Println(x)
+	fmt.Println(len(x))
+	fmt.Println(x[42])
+}
