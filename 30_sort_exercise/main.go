4dd1024c3e0e5c893e2c118a5c53d094dc286569
SpyR1014 <spyr1014@gmail.com>
2016-02-20 10:03:16 +1100

    Cleaned up the solution to question 1

--- 30_sort_exercise/main.go
+++ 30_sort_exercise/main.go
@@ -15,37 +15,15 @@ import (
 
 type people []string
 
-func (p people) Len() int {
-	// fmt.Println("length is", len(p))
-	return len(p)
-}
-func (p people) Swap(i, j int) {
-	// fmt.Println("swapping", p[i], "with", p[j])
-	p[i], p[j] = p[j], p[i]
-}
-func (p people) Less(i, j int) bool {
-	// fmt.Println("is", p[i], "less than", p[j])
-	return p[i] < p[j]
-}
-
-// Reverse sort by pretty much creating another type that has different methods.
-type reverse people
-
-// These three methods are a requirement of Interface interface for sort.
-func (l reverse) Len() int {
-	return len(l)
-}
-
-func (l reverse) Swap(i, j int) {
-	l[i], l[j] = l[j], l[i]
-}
-
-func (l reverse) Less(i, j int) bool { return l[i] > l[j] }
+// Setting up Interface interface for sort.
+func (p people) Len() int           { return len(p) }
+func (p people) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
+func (p people) Less(i, j int) bool { return p[i] < p[j] }
 
 func main() {
-	studyGroup := people{"Zuno", "Jahn", "Albert", "Jenny"}
+	studyGroup := people{"Zuno", "Derren", "Jahn", "Albert", "Jenny", "Rachel"}
 	fmt.Println(studyGroup)
-	sort.Sort(reverse(studyGroup))
+	sort.Sort(sort.Reverse(studyGroup)) // Using reverse interface
 	fmt.Println(studyGroup)
 	sort.Sort(studyGroup)
 	fmt.Println(studyGroup)
