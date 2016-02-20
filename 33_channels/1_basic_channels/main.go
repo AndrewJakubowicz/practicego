c7553f5564e8395426d4c5ac23d6c23699f96a5d
SpyR1014 <spyr1014@gmail.com>
2016-02-20 12:14:23 +1100

    Really cool simple channel example

---
+++ 33_channels/1_basic_channels/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+var wg sync.WaitGroup
+
+func main() {
+	c := make(chan int)
+	wg.Add(1)
+	go func() {
+		for i := 0; i < 20; i++ {
+			c <- i
+			// Code stops until the value is taken off the channel
+		}
+		wg.Done()
+	}()
+	go func() {
+		for {
+			fmt.Println(<-c) // Takes value off the channel.
+		}
+	}()
+	wg.Wait()
+}
