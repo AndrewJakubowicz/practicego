950a8a5a190108867b5a3f053abd6c81761250d3
SpyR1014 <spyr1014@gmail.com>
2016-04-22 14:41:20 +1000

    added first os.Args[0]

--- 1_moreExamples/02_echo1/echo1.go
+++ 1_moreExamples/02_echo1/echo1.go
@@ -8,6 +8,8 @@ import (
 
 func main() {
 	var s, sep string
+	fmt.Println("File than ran called:", os.Args[0])
+	// s, sep := "",  ""  // This is an alternative of the above code.
 	// os.Args[0] is the name of the command itself.
 	for i := 1; i < len(os.Args); i++ {
 		s += sep + os.Args[i]
